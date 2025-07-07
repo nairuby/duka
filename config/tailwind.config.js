96e2aa6af5a73da9a38dd663c8330c18050a2560
Annastacia <annetotoh@gmail.com>
2025-07-07 15:25:21 +0300

    Fix hotwire livereload

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        courier: ['"Courier Prime", monospace'],
+        geist: ['"Geist", sans-serif'],
       },
     },
   },
