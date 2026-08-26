508c6d1623c07856d0f2d96092fcab35cd4f78aa
judahsan <baraka.judah@gmail.com>
2026-08-26 14:52:43 +0300

    chore: update production mailer host configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "https://shop.rubycommunity.africa" }
+  config.action_mailer.default_url_options = { host: "shop.rubycommunity.africa" }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
